adcea871873c85bd83b6a7976c55e35874d5d05f
Mariia_Borovikova <mariia_borovikova@epam.com>
2018-10-15 11:46:30 +0300

    FLINK-5405: Add streaming connection to twitter through a internet proxy(2)

--- flink-connectors/flink-connector-twitter/src/main/java/org/apache/flink/streaming/connectors/twitter/TwitterSource.java
+++ flink-connectors/flink-connector-twitter/src/main/java/org/apache/flink/streaming/connectors/twitter/TwitterSource.java
@@ -129,25 +129,7 @@ public class TwitterSource extends RichSourceFunction<String> implements Stoppab
 			properties.getProperty(TOKEN_SECRET));
 
 		client = new ClientBuilder()
-			.name(properties.getProperty(CLIENT_NAME, "flink-twitter-source"))
-			.hosts(properties.getProperty(CLIENT_HOSTS, Constants.STREAM_HOST))
-			.endpoint(endpoint)
-			.authentication(auth)
-			.processor(new HosebirdMessageProcessor() {
-				public DelimitedStreamReader reader;
-
-				@Override
-				public void setup(InputStream input) {
-					reader = new DelimitedStreamReader(input, Constants.DEFAULT_CHARSET, Integer.parseInt(properties.getProperty(CLIENT_BUFFER_SIZE, "50000")));
-				}
-
-				@Override
-				public boolean process() throws IOException, InterruptedException {
-					String line = reader.readLine();
-					ctx.collect(line);
-					return true;
-				}
-			})
+			.proxy(properties.getProperty(CLIENT_HOSTS, Constants.STREAM_HOST), 0)
 			.build();
 
 		client.connect();
